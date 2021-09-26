963f86705d91d8375db7cf09bd5ef6134087a792
Mickael Gaspar <migatsar@gmail.com>
2021-09-27 00:15:52 +0200

    Added views for the app

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def detail(request, project_id):
+    context = {'project_id': project_id}
+    return render(request, 'detail.html', context)
