03cabb19ac598f4b7a5dac21193f194cf9515404
Mickael Gaspar <migatsar@gmail.com>
2021-09-27 18:34:51 +0200

    Debugged image field in model

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,9 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/images")
+    # image = models.FilePathField(path="/images")
+    # image = models.ImageField(upload_to='projects/static/images/')
+    image = models.ImageField(upload_to='projects')
 
     def __str__(self):
         return self.title
