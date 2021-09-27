b1f4540f7f0290706f53872f04506c8d421a7f3b
Mickael Gaspar <migatsar@gmail.com>
2021-09-27 14:19:48 +0200

    Tested different settings for TEMPLATES

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [],
+        # 'DIRS': [BASE_DIR / 'templates'],
+        # "DIRS": ["portfolio/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
