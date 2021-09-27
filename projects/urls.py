6c5c0d1ed95a62df3e5dd37b444487083f293e23
Mickael Gaspar <migatsar@gmail.com>
2021-09-27 02:50:26 +0200

    Completed URL paths

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
+    # ex: /projects/
     path('', views.index, name='index'),
+    # ex: /projects/5/
+    path('<int:project_id>/', views.detail, name='detail'),
 ]
